926b7c6f270d975e0f2590364ba3ded1769f4a7e
jamattes <jamattes@ncsu.edu>
2022-11-02 19:53:56 -0400

    Started adding victory conditions

---
+++ New Scenes/Condition/Condition.gd
@@ -0,0 +1,20 @@
+class_name Condition
+extends Resource
+
+## Superclass for victory and defeat conditions
+## This should allow for more flexibility
+## when creating new levels
+## However, I do worry that this may introduce
+## higher levels of coupling
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func _check_condition():
+	print("Condition function should be overwritten")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
