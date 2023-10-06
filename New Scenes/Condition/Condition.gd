00e289da309229fed583cc8a0e2f075e64c717da
jamattes <jamattes@ncsu.edu>
2023-10-06 17:59:32 -0400

    condition

--- New Scenes/Condition/Condition.gd
+++ New Scenes/Condition/Condition.gd
@@ -7,6 +7,13 @@ extends Resource
 ## However, I do worry that this may introduce
 ## higher levels of coupling
 
+enum condition {
+	ROUT,
+	KILL,
+	SURVIVE,
+	ESCAPE
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
