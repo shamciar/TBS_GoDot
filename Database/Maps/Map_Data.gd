932ead8137892a22cd26bef96321d2b1530950a9
jamattes <jamattes@ncsu.edu>
2023-11-13 20:16:24 -0500

    Map data

---
+++ Database/Maps/Map_Data.gd
@@ -0,0 +1,14 @@
+extends Resource
+
+## Class representing maps
+
+# Map information
+# Map name
+# Background
+# Player phase music
+# Enemy phase music
+
+# Grid information
+# Grid offset
+# There will probably need to be a tool somewhere in here
+# to allow for direct map editing in the editor
